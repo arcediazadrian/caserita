6feba63bca11a20007c1a483b97bb1a370e024ff
Paulo Aparicio <jose.apa4996@gmail.com>
2024-06-15 22:17:43 -0400

    Created Folder for Middlewares and added a Global Exception Handlers

---
+++ Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Middlewares
+{
+    public class GlobalExceptionHandlerMiddleware : IFunctionsWorkerMiddleware
+    {
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+        public GlobalExceptionHandlerMiddleware(ILogger<GlobalExceptionHandlerMiddleware> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+        {
+			try
+			{
+				await next(context);
+			}
+			catch (Exception ex)
+			{
+                _logger.LogError(ex, "Error processing invocation");
+
+                var httpReqData = await context.GetHttpRequestDataAsync();
+
+                if (httpReqData != null)
+                {
+                    // Create an instance of HttpResponseData with 500 status code.
+                    var newHttpResponse = httpReqData.CreateResponse(HttpStatusCode.InternalServerError);
+                    // You need to explicitly pass the status code in WriteAsJsonAsync method.
+                    // https://github.com/Azure/azure-functions-dotnet-worker/issues/776
+                    await newHttpResponse.WriteAsJsonAsync(new { FooStatus = "Invocation failed!" }, newHttpResponse.StatusCode);
+                    var invocationResult = context.GetInvocationResult();
+                    invocationResult.Value = newHttpResponse;
+                }
+            }
+        }
+    }
+}
